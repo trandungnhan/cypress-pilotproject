3782888e0d223fcb15c3669478b418abccd87f6d
Tran Dung Nhan <trandungnhan89@gmail.com>
2023-09-04 21:08:06 +0700

    add comment to trigger github actions

--- cypress/TheMagento/verifyWelcomeMegs.js
+++ cypress/TheMagento/verifyWelcomeMegs.js
@@ -1,5 +1,6 @@
 describe('Verify Welcome message', () => {
     
+    // Add note to trigger Github actions
     it('Verify Welcome message', () => {
         cy.visit("https://magento.softwaretestingboard.com/")        
         cy.get(".panel .not-logged-in").should('have.text', "Default welcome msg!")
